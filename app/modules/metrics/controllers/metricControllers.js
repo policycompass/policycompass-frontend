66d12f29fc9593c1034f2d60a0cb171cf82f1d27
Fabian Kirstein <fabian.kirstein@fokus.fraunhofer.de>
2014-07-17 09:52:44 +0200

    New Version of New Metric Form

--- app/modules/metrics/controllers/metricControllers.js
+++ app/modules/metrics/controllers/metricControllers.js
@@ -15,17 +15,30 @@ angular.module('pcApp.metrics.controllers.metric', [
 
 }])
 
-.controller('MetricDetailController', ['$scope', '$routeParams', '$location', 'Metric', function($scope, $routeParams, $location, Metric) {
-	this.message = "Hello";
+.controller('MetricDetailController', ['$scope', '$routeParams', '$location', 'Metric', '$log', function($scope, $routeParams, $location, Metric, $log) {
 
-    $scope.test = "hallo";
+    $scope.datagrid = [[]];
+    $scope.gridvisible = false;
+
 	$scope.metric = Metric.get({id: $routeParams.metricId},
-			function(metricList) {
+			function(metric) {
 			},
 			function(error) {
 				alert(error.data.message);
 			}
 	);
+
+    $scope.metric.$promise.then(function(metric){
+        var result = [];
+        $scope.metric.data.table.forEach(function (e) {
+            var row = [e.from, e.to, e.value];
+            result.push(row);
+        });
+        $log.info(result);
+        $scope.datagrid = result;
+        $scope.gridvisible = true;
+    });
+
 
     $scope.deleteMetric = function(metric) {
         metric.$delete(
@@ -40,43 +53,65 @@ angular.module('pcApp.metrics.controllers.metric', [
 
 .controller('MetricCreateController', ['$scope', 'Metric', '$location', '$log', function($scope, Metric, $location, $log) {
 
+    $scope.step = 'one';
+    $scope.columnselection = ['A','B','C','D','E','F','G'];
+
     $scope.datagrid = [
         []
     ];
 
     $scope.metric = {};
-    $scope.columns = {};
+    $scope.columns = {
+        from: 0,
+        to: 1,
+        value: 2
+    };
+
+    $scope.nextStep = function() {
+        validation();
+        $scope.step = 'second';
+    };
+
+    $scope.prevStep = function() {
+        $scope.step = 'one';
+    };
+
+    var validation = function() {
+
+    };
 
 
 	$scope.createMetric = function() {
         $log.info( $scope.datagrid[0]);
-        //$scope.stage = "second";
-
-        $scope.metric.unit = 1;
-        $scope.metric.language = 1;
         $scope.metric.user_id = 1;
 
-        $scope.metric.description = "Description";
-
         var data = [];
+        var extra = [];
+        if($scope.columns.category) {
+            extra.push($scope.columns.category);
+        }
+
         $scope.datagrid.forEach(function(e){
             if(e[0] != null){
-                data.push({
+                var row = {
                     from: e[$scope.columns.from],
                     to: e[$scope.columns.to],
                     value: e[$scope.columns.value]
-                });
+                };
+                if($scope.columns.category) {
+                    row[$scope.columns.category] = e[$scope.columns.extra];
+                }
+                data.push(row);
             }
         });
 
         $scope.metric.data = {
             table: data,
-            extra_columns: [
-            ]
+            extra_columns: extra
         };
 
-		Metric.save($scope.metric,function(){
-			$location.path('/metrics');
+		Metric.save($scope.metric,function(value, responseHeaders){
+			$location.path('/metrics/' + value.id);
 		},
 		function(err) {
             throw { message: err.data};
--- app/modules/metrics/directives/pcDatagrid.js
+++ app/modules/metrics/directives/pcDatagrid.js
@@ -2,21 +2,33 @@ angular.module('pcApp.metrics.directives.pcDatagrid', [
 
 ])
 
-.directive('pcDatagrid', function () {
+.directive('pcDatagrid', ['$timeout', '$log', function ($timeout, $log) {
     return {
-        restrict: 'E',
+        restrict: 'C',
         scope: {
-          gridData: '=data'
+            gridData: '=data',
+            readOnly: '=readOnly'
         },
-        link: function link(scope, element, attrs) {
-            $(element).handsontable({
-                data: scope.gridData,
-                colHeaders: true,
-                rowHeaders: true,
-                contextMenu: true,
-                minSpareRows: 10,
-                minSpareCols: 10
+        link: function (scope, element, attrs) {
+            angular.element(document).ready(function () {
+                var config = {
+                    data: scope.gridData,
+                    colHeaders: true,
+                    rowHeaders: true,
+                    contextMenu: true,
+                    stretchH: 'all',
+                    minRows: 10,
+                    minCols: 7,
+                    minSpareRows: 1
+                };
+
+                if(scope.readOnly == true) {
+                    config['readOnly'] = true;
+                }
+
+                $(element).handsontable(config);
             });
+
         }
     };
-});
+}]);
