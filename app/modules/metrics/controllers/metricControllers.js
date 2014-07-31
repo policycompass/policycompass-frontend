9d4c927c2ca1bddb18e59b8fbffeea1ad9793dc7
Fabian Kirstein <fabian.kirstein@fokus.fraunhofer.de>
2014-07-31 14:26:47 +0200

    Added Update Metric Update Page

--- app/modules/metrics/controllers/metricControllers.js
+++ app/modules/metrics/controllers/metricControllers.js
@@ -3,6 +3,28 @@ angular.module('pcApp.metrics.controllers.metric', [
     'pcApp.references.services.reference'
 ])
 
+.factory('helper', [function() {
+    return {
+        initCreateController: function($scope) {
+            $scope.step = 'one';
+            $scope.columnselection = ['A','B','C','D','E','F','G'];
+
+            $scope.nextStep = function() {
+                validation();
+                $scope.step = 'second';
+            };
+
+            $scope.prevStep = function() {
+                $scope.step = 'one';
+            };
+
+            var validation = function() {
+
+            };
+        }
+    };
+}])
+
 .controller('MetricsController', ['$scope', 'Metric', '$log', function($scope, Metric, $log) {
 	$log.info("hallo");
 	$scope.metrics = Metric.query(
@@ -36,18 +58,7 @@ angular.module('pcApp.metrics.controllers.metric', [
 	);
 
     $scope.metric.$promise.then(function(metric){
-        var result = [];
-
-        $scope.metric.data.table.forEach(function (e) {
-            var row_extras = [];
-            $scope.metric.data.extra_columns.forEach(function(extra){
-                row_extras.push(e[extra]);
-            });
-            var row = [e.from, e.to, e.value];
-            row = row.concat(row_extras);
-            result.push(row);
-        });
-        $scope.datagrid = result;
+        $scope.datagrid = metric.getDataAsGrid();
         $scope.gridvisible = true;
     });
 
@@ -68,11 +79,14 @@ angular.module('pcApp.metrics.controllers.metric', [
         'Metric',
         '$location',
         '$log',
-        function($scope, Metric, $location, $log) {
+        'helper',
+        function($scope, Metric, $location, $log, helper) {
 
+    helper.initCreateController($scope);
 
-    $scope.step = 'one';
-    $scope.columnselection = ['A','B','C','D','E','F','G'];
+    $scope.mode = "create";
+    $scope.gridvisible = true;
+
 
     $scope.datagrid = [
         []
@@ -84,20 +98,6 @@ angular.module('pcApp.metrics.controllers.metric', [
         to: 1,
         value: 2
     };
-
-    $scope.nextStep = function() {
-        validation();
-        $scope.step = 'second';
-    };
-
-    $scope.prevStep = function() {
-        $scope.step = 'one';
-    };
-
-    var validation = function() {
-
-    };
-
 
 	$scope.createMetric = function() {
         $scope.metric.user_id = 1;
@@ -136,4 +136,87 @@ angular.module('pcApp.metrics.controllers.metric', [
 
 		);
 	};
+}])
+
+.controller('MetricEditController', [
+        '$scope',
+        '$routeParams',
+        'Metric',
+        '$location',
+        '$log',
+        'helper',
+    function($scope, $routeParams, Metric, $location, $log, helper) {
+
+        helper.initCreateController($scope);
+        $scope.mode = "edit";
+        $scope.gridvisible = false;
+
+        $scope.datagrid = [
+            []
+        ];
+
+        $scope.metric = Metric.get({id: $routeParams.metricId},
+            function(metric) {
+            },
+            function(error) {
+                alert(error.data.message);
+            }
+        );
+
+        $scope.metric.$promise.then(function(metric){
+            $scope.metric.unit = $scope.metric.unit.id;
+            $scope.metric.language = $scope.metric.language.id;
+            $scope.datagrid = metric.getDataAsGrid();
+            $scope.gridvisible = true;
+            $scope.metric.policyDomain = ["1","2"];
+            $scope.metric.external_resource = 1;
+            $scope.columns.category =  $scope.metric.data.extra_columns[0];
+        });
+
+
+        $scope.columns = {
+            from: 0,
+            to: 1,
+            value: 2,
+            extra: 3
+        };
+
+
+        $scope.createMetric = function() {
+            $scope.metric.user_id = 1;
+
+            var data = [];
+            var extra = [];
+            if($scope.columns.category) {
+                extra.push($scope.columns.category);
+            }
+
+            $scope.datagrid.forEach(function(e){
+                if(e[0] != null){
+                    var row = {
+                        from: e[$scope.columns.from],
+                        to: e[$scope.columns.to],
+                        value: e[$scope.columns.value]
+                    };
+                    if($scope.columns.category) {
+                        row[$scope.columns.category] = e[$scope.columns.extra];
+                    }
+                    data.push(row);
+                }
+            });
+
+            $scope.metric.data = {
+                table: data,
+                extra_columns: extra
+            };
+
+            Metric.update($scope.metric,function(value, responseHeaders){
+                    $location.path('/metrics/' + value.id);
+                },
+                function(err) {
+                    throw { message: err.data};
+                }
+
+            );
+        };
 }]);
--- app/modules/metrics/services/metricService.js
+++ app/modules/metrics/services/metricService.js
@@ -8,8 +8,25 @@ angular.module('pcApp.metrics.services.metric',[
 	var Metric = $resource(url,
 		{
 			id: "@id"
-		}
+		},
+        {
+            'update': { method:'PUT' }
+        }
 	);
+
+    Metric.prototype.getDataAsGrid =  function(){
+        var result = [];
+        this.data.table.forEach(function (e) {
+            var row_extras = [];
+            this.data.extra_columns.forEach(function(extra){
+                row_extras.push(e[extra]);
+            });
+            var row = [e.from, e.to, e.value];
+            row = row.concat(row_extras);
+            result.push(row);
+        }, this);
+        return result;
+    };
 	return Metric;
 }]);
 
