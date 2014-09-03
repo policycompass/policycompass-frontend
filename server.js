d22b6a58298e472653610ccabef231bc3d2d10c8
Nicolas Dietrich <nidi@mailbox.org>
2014-09-03 19:51:55 +0200

    remove whitespace

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 /**
  * Policy Compass Portal Development Server
- * v0.1 
- * 
+ * v0.1
+ *
  * Author: Fabian Kirstein 2014
- * 
+ *
  * This is a small Node.js web server for development.
  * It includes a Proxy functionality to proxy the external services. This
- * is necessary due to the Same-Origin-Policy. 
- * 
+ * is necessary due to the Same-Origin-Policy.
+ *
  * Configuration: Put the domains of the services in the development.json
- * Start: node server.js [port]  
- * 
+ * Start: node server.js [port]
+ *
  * Default port is 8000
- * 
+ *
  */
 var http = require("http"),
 	url = require("url"),
@@ -33,10 +33,10 @@ http.createServer(function(request, response) {
 
 	var uri = url.parse(request.url).pathname
 	, filename = path.join(process.cwd(), uri);
-	
+
 	console.log('[%s] "%s %s" "%s"', (new Date).toUTCString(), request.method, request.url, request.headers['user-agent']);
-	
-	
+
+
 	//Proxy all requests for the metrics service to the Data Manager
 	// /api/v*/metrics
 	if (/^\/api\/v[0-9]+\/metricsmanager/.exec(request.url)) {
@@ -46,7 +46,7 @@ http.createServer(function(request, response) {
 	} else if (/^\/api\/v[0-9]+\/visualizationsmanager/.exec(request.url)) {
 	    proxy.web(request, response, {
 	      target: pcServicesUrl
-	    });	
+	    });
 	} else if (/^\/api\/v[0-9]+\/eventsmanager/.exec(request.url)) {
         proxy.web(request, response, {
             target: pcServicesUrl
@@ -57,7 +57,7 @@ http.createServer(function(request, response) {
         });
     }
     else {
-		
+
 		fs.exists(filename, function(exists) {
 			if(!exists) {
 				response.writeHead(404, {"Content-Type": "text/plain"});
@@ -81,9 +81,9 @@ http.createServer(function(request, response) {
 				response.end();
 			});
 		});
-		
+
 	}
-		
+
 }).listen(parseInt(port, 10));
 
 console.log("Static file server running at\n => http://localhost:" + port + "/\nCTRL + C to shutdown");
