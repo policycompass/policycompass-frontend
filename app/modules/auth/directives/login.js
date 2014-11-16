260b2223da117d3bdbe7b23af7fc0d0c1fb77e88
Nicolas Dietrich <nidi@mailbox.org>
2014-11-16 21:03:30 +0100

    Add adh-cross-window-channel directive

--- app/modules/auth/directives/login.js
+++ app/modules/auth/directives/login.js
@@ -31,4 +31,14 @@ angular.module('pcApp.auth.directives.login', [
             });
         }
     };
+}])
+.directive('adhCrossWindowChannel', ['Adhocracy', function (Adhocracy) {
+    return {
+        restrict: 'E',
+        link: function(scope, element, attrs) {
+            Adhocracy.then(function (adh) {
+                element.append(adh.getIframe('empty', {noheader: true, nocenter: true}))
+            });
+        }
+    };
 }]);
