b294c33667c933c537b50bf83204003e668b18ec
unknown <S229570@MC0QZMRC.ww930.my-it-solutions.net>
2015-04-28 14:46:16 +0200

    frontend 1.1

--- app/modules/common/design.js
+++ app/modules/common/design.js
@@ -1,5 +1,19 @@
 
+// alert('test');
+// $('.switch-description').css('cursor','pointer');
 
 
+// $('.switch-description').click(function(){
+        // var $body = $(this).next();
+        // $('.summary').not($body).removeClass('active');
+        // $(this).next(.toggleClass('active');
+    // });
 
-
+// $('.summary-switch').click(function(){
+        // var $body = $(this).next();
+        // $('.summary').not($body).removeClass('active');
+        // $($body).toggleClass('active');
+    // });
+function MainController($scope) {
+  $scope.toggle = true;
+}
