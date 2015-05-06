0fbaae5acc416959a99bf5bb3fbb56a04ab99d7c
Alejandro Barrio <alejandro.barrio@atos.net>
2015-05-06 11:31:23 +0200

    Delete design.js link and other files

--- app/modules/common/config.rb
+++
@@ -1,7 +0,0 @@
-#config.rb
-css_dir = "css" # Carpeta en donde iran los .css
-sass_dir = "sass" # Carpeta en donde iran los .scss
-# extensions_dir  = "sass-extensions"
-# images_dir = "images" # Carpeta en donde van las imagenes
-# javascripts_dir = "js"
-output_style = :expanded # :expanded, :nested, :compact, :compressed
