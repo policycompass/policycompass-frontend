5ccaef5c8a3924a675ee8b3ae35ec6d0b99022ca
Nicolas Dietrich <nidi@mailbox.org>
2015-09-09 01:50:39 +0200

    Expose user name and admin state in auth module
    Due to liqd/adhocracy3#1635, userData isn't submitted properly anymore.
    Therefore we fetch the user again on policycompass side in order to
    retrieve the username. This can be removed later.
    
    In addition, we now set Auth.state.isAdmin if the user is an admin user
    (i.e. belongs to the gods group and thus has the god role in Adhocracy).
    
    Fixes policycompass/policycompass#121

--- app/modules/auth/services/auth.js
+++ app/modules/auth/services/auth.js
@@ -2,26 +2,47 @@ angular.module('pcApp.auth.services.auth', [
     'pcApp.adhocracyEmbedder.services.adhocracy'
 ])
 
-.factory('Auth', ['Adhocracy', '$rootScope', '$http', '$location', function (Adhocracy, $rootScope, $http, $location) {
+.factory('Auth', ['Adhocracy', 'API_CONF', '$rootScope', '$http', '$location', '$q', function (Adhocracy, API_CONF, $rootScope, $http, $location, $q) {
 
     var Auth = {
         state: {
             loggedIn: undefined,
-            username: undefined,
-            userData: undefined
+            userData: undefined,
+            isAdmin: undefined
         },
 
         // NOTE: _login and _logout are only meant to be called through
         // the AdhocracySDK.
 
         _login: function (userData, token, userPath) {
-            _.defer(function () {
-                $rootScope.$apply(function () {
-                    Auth.state.loggedIn = true;
-                    Auth.state.userData = userData;
+            // FIXME: Due to liqd/adhocracy3#1635, we cannot use userData, but
+            // need to fetch the user object manually again here. This can be
+            // removed once that is fixed.
+            $q.all([
+                $http.get(userPath),
+                $http.get(API_CONF.ADHOCRACY_BACKEND_URL + "/principals/groups/gods/")
+            ]).then(function (responses) {
+                var user = responses[0].data;
+                var username = user.data["adhocracy_core.sheets.principal.IUserBasic"].name;
+                var godsGroup = responses[1].data;
+                var godsGroupSheet = godsGroup.data["adhocracy_core.sheets.principal.IGroup"];
+                var isAdmin = (_.contains(godsGroupSheet.roles, "god") && _.contains(godsGroupSheet.users, userPath));
 
-                    $http.defaults.headers.common["X-User-Token"] = token;
-                    $http.defaults.headers.common["X-User-Path"] = userPath;
+                _.defer(function () {
+                    $rootScope.$apply(function () {
+                        Auth.state.loggedIn = true;
+                        if (typeof userData !== "undefined") {
+                            Auth.state.userData = userData;
+                        } else {
+                            Auth.state.userData = {
+                                name: username
+                            };
+                        }
+                        Auth.state.isAdmin = isAdmin;
+
+                        $http.defaults.headers.common["X-User-Token"] = token;
+                        $http.defaults.headers.common["X-User-Path"] = userPath;
+                    });
                 });
             });
         },
@@ -30,7 +51,8 @@ angular.module('pcApp.auth.services.auth', [
             _.defer(function () {
                 $rootScope.$apply(function () {
                     Auth.state.loggedIn = false;
-                    Auth.state.UserData = undefined;
+                    Auth.state.userData = undefined;
+                    Auth.state.isAdmin = false;
 
                     delete $http.defaults.headers.common["X-User-Token"];
                     delete $http.defaults.headers.common["X-User-Path"];
