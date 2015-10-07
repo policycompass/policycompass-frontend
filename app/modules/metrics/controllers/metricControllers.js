6bce4f08a04283f4f59e225803dc4947567d9932
Magdalena Noffke <magdalena.noffke@googlemail.com>
2015-10-07 10:34:54 +0200

    remove unused code

--- app/modules/metrics/controllers/metricControllers.js
+++ app/modules/metrics/controllers/metricControllers.js
@@ -28,13 +28,9 @@ angular.module('pcApp.metrics.controllers.metric', [
                 $scope.metrics_controller_helper.metricsdata.formula = "";
             }
             $scope.metrics_controller_helper.metricsdata.formula = $scope.metrics_controller_helper.metricsdata.formula + i;
-            //$("#formula-div").append('<span contentEditable="false" class="indicator-formula indicator-formula-selected">' + indicator.name + '</span>');
-            //$("#formula-div").css('height', ($("#formula-div")[0].scrollHeight + 19) +  'px');
         }
         else{
             $scope.metrics_controller_helper.metricsdata.formula = [$scope.metrics_controller_helper.metricsdata.formula.slice(0, $scope.cursorPosVal), i, $scope.metrics_controller_helper.metricsdata.formula.slice($scope.cursorPosVal)].join('');
-            //$("#formula-div").append('<span contentEditable="false" class="indicator-formula indicator-formula-selected">'+ indicator.name +'</span>' );
-            //$("#formula-div").css('height',($("#formula-div")[0].scrollHeight + 19) + 'px');
             $scope.cursorPosVal = undefined;
         }
         $scope.variableIndex += 1;
@@ -44,29 +40,6 @@ angular.module('pcApp.metrics.controllers.metric', [
     $scope.getCursorPosition = function(event) {
         var element = event.target;
         $scope.doGetCaretPosition(element);
-
-        /*var caretOffset = 0;
-        var doc = element.ownerDocument || element.document;
-        var win = doc.defaultView || doc.parentWindow;
-        var sel;
-        if (typeof win.getSelection != "undefined") {
-            sel = win.getSelection();
-            if (sel.rangeCount > 0) {
-                var range = win.getSelection().getRangeAt(0);
-                var preCaretRange = range.cloneRange();
-                preCaretRange.selectNodeContents(element);
-                preCaretRange.setEnd(range.endContainer, range.endOffset);
-                caretOffset = preCaretRange.toString().length;
-            }
-        } else if ( (sel = doc.selection) && sel.type != "Control") {
-            var textRange = sel.createRange();
-            var preCaretTextRange = doc.body.createTextRange();
-            preCaretTextRange.moveToElementText(element);
-            preCaretTextRange.setEndPoint("EndToEnd", textRange);
-            caretOffset = preCaretTextRange.text.length;
-        }
-        $scope.cursorPosVal = caretOffset;
-        console.log($scope.cursorPosVal);*/
     };
 
     $scope.clearErrors = function() {
