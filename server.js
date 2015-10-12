8a2ec1bc5c24fc763b3ec3cf411bc7b4c463c2d3
Yves Müller <yves.mueller@liqd.de>
2015-10-12 15:29:40 +0200

    Fix indent in server.js

--- server.js
+++ server.js
@@ -15,13 +15,13 @@
  *
  */
 var http = require("http"),
-	url = require("url"),
-	path = require("path"),
-	fs = require("fs"),
-	httpProxy = require('http-proxy'),
-	nconf = require('nconf'),
-    mime = require('mime'),
-	port = process.argv[2] || 9000;
+url = require("url"),
+path = require("path"),
+fs = require("fs"),
+httpProxy = require('http-proxy'),
+nconf = require('nconf'),
+mime = require('mime'),
+port = process.argv[2] || 9000;
 
 nconf.file('development.json');
 
@@ -34,85 +34,85 @@ var proxy = httpProxy.createServer();
 // Heavily inspired by this Gist: https://gist.github.com/rpflorence/701407
 http.createServer(function(request, response) {
 
-	var uri = url.parse(request.url).pathname
-	, filename = path.join(process.cwd(), uri);
+  var uri = url.parse(request.url).pathname,
+  filename = path.join(process.cwd(), uri);
 
-	console.log('[%s] "%s %s" "%s"', (new Date).toUTCString(), request.method, request.url, request.headers['user-agent']);
+  console.log('[%s] "%s %s" "%s"', (new Date).toUTCString(), request.method, request.url, request.headers['user-agent']);
 
-	//Proxy all requests for the metrics service to the Data Manager
-	// /api/v*/metrics
-	if (/^\/api\/v[0-9]+\/metricsmanager/.exec(request.url)) {
-	    proxy.web(request, response, {
-	      target: pcServicesUrl
-	    });
-	} else if (/^\/api\/v[0-9]+\/datasetmanager/.exec(request.url)) {
-	    proxy.web(request, response, {
-	      target: pcServicesUrl
-	    });   
-	} else if (/^\/api\/v[0-9]+\/visualizationsmanager/.exec(request.url)) {
-	    proxy.web(request, response, {
-	      target: pcServicesUrl
-	    });   
-	} else if (/^\/api\/v[0-9]+\/indicatorservice/.exec(request.url)) {
-	    proxy.web(request, response, {
-	      target: pcServicesUrl
-	    }); 	    
-	} else if (/^\/api\/v[0-9]+\/eventsmanager/.exec(request.url)) {
-        proxy.web(request, response, {
-            target: pcServicesUrl
-        });
-    } else if (/^\/api\/v[0-9]+\/references/.exec(request.url)) {
-        proxy.web(request, response, {
-            target: pcServicesUrl
-        });
-    } else if (/^\/api\/v[0-9]+\/searchmanager/.exec(request.url)) {
-        proxy.web(request, response, {
-            target: pcServicesUrl
-        });
-    } else if (/^\/api\/v[0-9]+\/fcmmanager/.exec(request.url)) {
-        proxy.web(request, response, {
-            target: fcmServicesUrl
-        });
-    } else if (/^\/policycompass_search/.exec(request.url)) {
-        proxy.web(request, response, {
-            target: elasticSearchUrl
-        });
-    } else if (/^\/(app|)$/.exec(request.url)) {
-        response.writeHead(302, {"location": "/app/"});
-        response.end();
-    }
-	else if (/^\/media/.exec(request.url)) {
-	    proxy.web(request, response, {
-	      target: pcServicesUrl
-	    });
+  //Proxy all requests for the metrics service to the Data Manager
+  // /api/v*/metrics
+  if (/^\/api\/v[0-9]+\/metricsmanager/.exec(request.url)) {
+    proxy.web(request, response, {
+      target: pcServicesUrl
+    });
+  } else if (/^\/api\/v[0-9]+\/datasetmanager/.exec(request.url)) {
+    proxy.web(request, response, {
+      target: pcServicesUrl
+    });
+  } else if (/^\/api\/v[0-9]+\/visualizationsmanager/.exec(request.url)) {
+    proxy.web(request, response, {
+      target: pcServicesUrl
+    });
+  } else if (/^\/api\/v[0-9]+\/indicatorservice/.exec(request.url)) {
+    proxy.web(request, response, {
+      target: pcServicesUrl
+    });
+  } else if (/^\/api\/v[0-9]+\/eventsmanager/.exec(request.url)) {
+    proxy.web(request, response, {
+      target: pcServicesUrl
+    });
+  } else if (/^\/api\/v[0-9]+\/references/.exec(request.url)) {
+    proxy.web(request, response, {
+      target: pcServicesUrl
+    });
+  } else if (/^\/api\/v[0-9]+\/searchmanager/.exec(request.url)) {
+    proxy.web(request, response, {
+      target: pcServicesUrl
+    });
+  } else if (/^\/api\/v[0-9]+\/fcmmanager/.exec(request.url)) {
+    proxy.web(request, response, {
+      target: fcmServicesUrl
+    });
+  } else if (/^\/policycompass_search/.exec(request.url)) {
+    proxy.web(request, response, {
+      target: elasticSearchUrl
+    });
+  } else if (/^\/(app|)$/.exec(request.url)) {
+    response.writeHead(302, {"location": "/app/"});
+    response.end();
+  }
+  else if (/^\/media/.exec(request.url)) {
+    proxy.web(request, response, {
+      target: pcServicesUrl
+    });
+  }
+  else {
+
+    fs.exists(filename, function(exists) {
+      if(!exists) {
+	response.writeHead(404, {"Content-Type": "text/plain"});
+	response.write("404 Not Found\n");
+	response.end();
+	return;
+      }
+
+      if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+      fs.readFile(filename, "binary", function(err, file) {
+	if(err) {
+	  response.writeHead(500, {"Content-Type": "text/plain"});
+	  response.write(err + "\n");
+	  response.end();
+	  return;
 	}
-    else {
 
-		fs.exists(filename, function(exists) {
-			if(!exists) {
-				response.writeHead(404, {"Content-Type": "text/plain"});
-				response.write("404 Not Found\n");
-				response.end();
-				return;
-			}
+        response.writeHead(200, {"Content-Type": mime.lookup(filename)});
+	response.write(file, "binary");
+	response.end();
+      });
+    });
 
-			if (fs.statSync(filename).isDirectory()) filename += '/index.html';
-
-			fs.readFile(filename, "binary", function(err, file) {
-				if(err) {
-					response.writeHead(500, {"Content-Type": "text/plain"});
-					response.write(err + "\n");
-					response.end();
-					return;
-				}
-
-                response.writeHead(200, {"Content-Type": mime.lookup(filename)});
-				response.write(file, "binary");
-				response.end();
-			});
-		});
-
-	}
+  }
 
 }).listen(parseInt(port, 10));
 
