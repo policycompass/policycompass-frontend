fcdb6a954e512e48f5a581e7377146b5bba56ce2
Christos Botsikas <cbot@epu.ntua.gr>
2015-10-23 02:10:52 +0300

    minify CSS on production servers https://github.com/policycompass/policycompass/issues/308

--- config.rb
+++ config.rb
@@ -5,4 +5,4 @@ sass_dir = "app/sass"
 images_dir = "app/img" 
 javascripts_dir = "app/js"
 sourcemap = (environment == :production) ? false : true
-output_style = :expanded # :expanded, :nested, :compact, :compressed
+output_style = (environment == :production) ? :compressed : :expanded # :expanded, :nested, :compact, :compressed
