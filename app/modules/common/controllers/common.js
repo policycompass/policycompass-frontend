a84d0cb27ec4b3706534cf0c7ae4c6af8cab3f47
Andreas C. Osowski <andreas.osowski@fokus.fraunhofer.de>
2015-11-11 11:58:44 +0100

    Fixed Progress bar behaviour

--- app/modules/common/controllers/common.js
+++ app/modules/common/controllers/common.js
@@ -2,156 +2,153 @@
  * Controllers for common purposes
  */
 
-angular.module('pcApp.common.controllers', [
+angular.module('pcApp.common.controllers', [])
 
-])
+    /****
+     Used to search metrics using ekasticsearch api
+     ******/
+    /*
+     .factory('SearchMetrics',  ['$resource', 'API_CONF', function($resource, API_CONF) {
+     var url = "/"+API_CONF.ELASTIC_INDEX_NAME+'/:type/_search';
+     //var url = 'http://'+API_CONF.ELASTIC_URL + "/" + API_CONF.ELASTIC_INDEX_NAME + '/:type/_search';
+     //console.log("url");
+     //console.log(url);
 
-/****
-Used to search metrics using ekasticsearch api
-******/
-/*
-.factory('SearchMetrics',  ['$resource', 'API_CONF', function($resource, API_CONF) {
-	var url = "/"+API_CONF.ELASTIC_INDEX_NAME+'/:type/_search';
-	//var url = 'http://'+API_CONF.ELASTIC_URL + "/" + API_CONF.ELASTIC_INDEX_NAME + '/:type/_search';
-	//console.log("url");
-	//console.log(url);
-	
-	var SearchMetrics = $resource(url,
-		{		
-			search: "@search",
-			type: "@type"
-		},
-        {
-            'update': { method:'PUT' },
-            'post': { method:'POST', isArray:false },
-            'query': { method: 'GET', isArray:false}
-            //'query': { method: 'POST', isArray:false}
-        }
-	);
-	return SearchMetrics;
-}])
-*/
+     var SearchMetrics = $resource(url,
+     {
+     search: "@search",
+     type: "@type"
+     },
+     {
+     'update': { method:'PUT' },
+     'post': { method:'POST', isArray:false },
+     'query': { method: 'GET', isArray:false}
+     //'query': { method: 'POST', isArray:false}
+     }
+     );
+     return SearchMetrics;
+     }])
+     */
 
-.factory('Progress', function (ngProgress, $http) {
-    var timer;
-    return {    	
-        start: function () {
-        	ngProgress.color('#f6921e');
-            var me = this;
-            // reset the status of the progress bar
-            me.reset();
-            // if the `complete` method is not called
-            // complete the progress of the bar after 5 seconds
-            timer = setTimeout(function () {
-                me.complete();
-            }, 5000);
-            
-            $http({method: 'GET', url: '/app'})
-      		.success(function(data, status, headers, config) {
-          		me.complete();
-       		});
-            
-        },
-        complete: function () {
-            ngProgress.complete();
-            if (timer) {
-                // remove the 5 second timer
-                clearTimeout(timer);
-                timer = null;
+    .factory('Progress', function (ngProgress, $http) {
+        var timer;
+        ngProgress.color('#f6921e');
+        return {
+            start: function () {
+                if (timer) return;
+
+                var me = this;
+                // reset the status of the progress bar
+                me.reset();
+                // start the progress bar
+                ngProgress.start();
+                // if the `complete` method is not called
+                // complete the progress of the bar after 5 seconds
+                timer = setTimeout(function () {
+                    me.complete();
+                }, 5000);
+
+
+            },
+            complete: function () {
+                if (timer) {
+                    ngProgress.complete();
+                    // remove the 5 second timer
+                    clearTimeout(timer);
+                    timer = null;
+                }
+            },
+            reset: function () {
+                if (timer) {
+                    // remove the 5 second timer
+                    clearTimeout(timer);
+                    timer = null;
+                    // reset the progress bar
+                    ngProgress.reset();
+                }
             }
-        },
-        reset: function () {             
-            if (timer) {
-                // remove the 5 second timer
-                clearTimeout(timer);
-                // reset the progress bar
-                ngProgress.reset();
-            }
-            // start the progress bar
-            ngProgress.start();
         }
-    };
-})
-     
+    })
 
-.controller('CommonController', ['$scope', '$location', function($scope, $location) {
-    $scope.isActive = function (viewLocation) {
-        return viewLocation === $location.path();
-    };
-    $scope.isCollapsed = true;
-    $scope.navCollapsed = true;
-    $scope.toggleCreateMenu = function () {
-        $scope.isCollapsed = !$scope.isCollapsed
-    }
-    $scope.collapseCreateMenu = function () {
+
+    .controller('CommonController', ['$scope', '$location', function ($scope, $location) {
+        $scope.isActive = function (viewLocation) {
+            return viewLocation === $location.path();
+        };
         $scope.isCollapsed = true;
-    }
-    $scope.expandCreateMenu = function () {
-        $scope.isCollapsed = false;
-    }
-    /* Functions for Submenu Collapse.*/
-    $(document).on('click',function(e) {
-        var link= e.target.id;
-        if((link!=="createLink") && ($scope.isCollapsed == false)){
+        $scope.navCollapsed = true;
+        $scope.toggleCreateMenu = function () {
+            $scope.isCollapsed = !$scope.isCollapsed
+        }
+        $scope.collapseCreateMenu = function () {
             $scope.isCollapsed = true;
         }
-    });
-    /* Functions for login Button to call a route.*/
-    $scope.go = function ( path ) {
-        $location.path( path );
-    };
-}])
+        $scope.expandCreateMenu = function () {
+            $scope.isCollapsed = false;
+        }
+        /* Functions for Submenu Collapse.*/
+        $(document).on('click', function (e) {
+            var link = e.target.id;
+            if ((link !== "createLink") && ($scope.isCollapsed == false)) {
+                $scope.isCollapsed = true;
+            }
+        });
+        /* Functions for login Button to call a route.*/
+        $scope.go = function (path) {
+            $location.path(path);
+        };
+    }])
 
-.controller('StaticController', ['$scope', '$modal', function($scope, $modal) {
-
-
-}])
-
-.controller('wanttoController', ['$scope', '$rootScope', '$modal', function($scope, $rootScope, $modal) {
-	$rootScope.wizard_help = false;
-}])
-
-.controller('CreateDataset2Controller', ['$scope', '$modal', function($scope, $modal) {
-	console.log("CreateDataset2Controller");
-
-$scope.classtypedata = [
-    { icon: "",   name: "Country",    maker: "",        ticked: false  },
-    { icon: "",   name: "Age Class",  maker: "",        ticked: false },
-    { icon: "",   name: "Gender",     maker: "",    	ticked: false  }
-]; 
-$scope.classtypedata2 = [
-    { icon: "",   name: "Country",    maker: "",        ticked: false  },
-    { icon: "",   name: "Age Class",  maker: "",        ticked: false },
-    { icon: "",   name: "Gender",     maker: "",    	ticked: false  }
-]; 
-
-$scope.gendermultiselect = [
-    { icon: "",   name: "Male",    maker: "",        ticked: false  },
-    { icon: "",   name: "Female",  maker: "",        ticked: false },
-    { icon: "",   name: "Death Rate",     maker: "",    	ticked: false  }
-];
+    .controller('StaticController', ['$scope', '$modal', function ($scope, $modal) {
 
 
-}])	
+    }])
 
-/**
- * Controller for setting the UI Bootstrap date selection.
- * ToDo: Should be a directive.
- */
-.controller('DateController',  ['$scope', function ($scope) {
+    .controller('wanttoController', ['$scope', '$rootScope', '$modal', function ($scope, $rootScope, $modal) {
+        $rootScope.wizard_help = false;
+    }])
 
-    $scope.maxDate = new Date();
-    $scope.minDate = new Date('1900-01-01');
-    $scope.format = "yyyy-MM-dd";
-    $scope.dateOptions = {
-        formatYear: 'yyyy',
-        startingDay: 1
-    };
+    .controller('CreateDataset2Controller', ['$scope', '$modal', function ($scope, $modal) {
+        console.log("CreateDataset2Controller");
 
-    $scope.open = function($event) {
-        $event.preventDefault();
-        $event.stopPropagation();
-        $scope.opened = true;
-    };
+        $scope.classtypedata = [
+            {icon: "", name: "Country", maker: "", ticked: false},
+            {icon: "", name: "Age Class", maker: "", ticked: false},
+            {icon: "", name: "Gender", maker: "", ticked: false}
+        ];
+        $scope.classtypedata2 = [
+            {icon: "", name: "Country", maker: "", ticked: false},
+            {icon: "", name: "Age Class", maker: "", ticked: false},
+            {icon: "", name: "Gender", maker: "", ticked: false}
+        ];
 
-}]);
+        $scope.gendermultiselect = [
+            {icon: "", name: "Male", maker: "", ticked: false},
+            {icon: "", name: "Female", maker: "", ticked: false},
+            {icon: "", name: "Death Rate", maker: "", ticked: false}
+        ];
+
+
+    }])
+
+    /**
+     * Controller for setting the UI Bootstrap date selection.
+     * ToDo: Should be a directive.
+     */
+    .controller('DateController', ['$scope', function ($scope) {
+
+        $scope.maxDate = new Date();
+        $scope.minDate = new Date('1900-01-01');
+        $scope.format = "yyyy-MM-dd";
+        $scope.dateOptions = {
+            formatYear: 'yyyy',
+            startingDay: 1
+        };
+
+        $scope.open = function ($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            $scope.opened = true;
+        };
+
+    }]);
