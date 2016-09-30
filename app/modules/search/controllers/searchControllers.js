0cfd82ae80f20bd9c5a00edd3a69194e308180e2
Michael <michael.alemu@gmail.com>
2016-09-30 14:05:39 +0200

    added a textfield to filter within filters

--- app/modules/search/controllers/searchControllers.js
+++ app/modules/search/controllers/searchControllers.js
@@ -644,5 +644,18 @@
 
 
     searchmodule.controller("searchMainController", searchMainController);
+    searchmodule.filter('toArray', function () {
+        'use strict';
+
+        return function (obj) {
+            if (!(obj instanceof Object)) {
+                return obj;
+            }
+
+            return Object.keys(obj).map(function (key) {
+                return Object.defineProperty(obj[key], '$key', {__proto__: null, value: key});
+            });
+        }
+    });
 
 }());
